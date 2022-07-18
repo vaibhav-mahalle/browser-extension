c4b3a33bb2b3e6779d8adbf2ade2323c341b5297
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-07-18 19:22:58 +0530

    feat, style: added styles and components

--- extension-app/src/components/CoverImage.jsx
+++ extension-app/src/components/CoverImage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { getCoverImage } from "../utils/api-requests";
 
-export const CoverImage = () => {
+const CoverImage = () => {
+  const initialData = {
+    url: "",
+    location: "",
+    description: "",
+  };
+  const [imageData, setImageData] = useState(initialData);
+  const { url, location, description } = imageData;
+
+  useEffect(() => {
+    getCoverImage(setImageData);
+    setInterval(() => getCoverImage(setImageData), 1800000);
+  }, []);
+
   return (
-    <div>CoverImage</div>
-  )
-}
+    <div>
+      <img className="cover-img" src={url} alt={description} />
+      <div className="img-details text-light">
+        <small>{location}</small>
+      </div>
+    </div>
+  );
+};
+export { CoverImage };
