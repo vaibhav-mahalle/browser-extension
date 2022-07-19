9ea5f394b9f7bd78de8cc3527aa48c5bc82e25aa
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-07-19 13:10:15 +0530

    feat: added quote and weather component

--- extension-app/src/components/Weather.jsx
+++ extension-app/src/components/Weather.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { getWeatherUpdate } from "../utils/api-requests";
 
-export const Weather = () => {
+const Weather = () => {
+  const [weather, setWeather] = useState({
+    city: "",
+    temp: "",
+    icon: "",
+    desc: "",
+  });
+
+  useEffect(() => {
+    getWeatherUpdate(setWeather);
+    const timer = setInterval(() => getWeatherUpdate(setWeather), 3600000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div>Weather</div>
-  )
-}
+    <div className="weather-container">
+      <img className="weather-icon" src={weather.icon} />
+      <div>
+        <h2 className="weather-text text-light">{weather.temp}</h2>
+        <small className="weather-text text-light">{weather.city}</small>
+        <p></p>
+        <small className="weather-text text-light">{weather.desc}</small>
+      </div>
+    </div>
+  );
+};
+export { Weather };
